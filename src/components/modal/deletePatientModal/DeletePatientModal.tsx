1d528938184ad3f7c0fffb746e2abc71e3654147
HieuBM <buiminhhieusct@gmail.com>
2024-11-01 15:10:36 +0700

    delete patient

--- src/components/modal/deletePatientModal/DeletePatientModal.tsx
+++ src/components/modal/deletePatientModal/DeletePatientModal.tsx
@@ -13,8 +13,19 @@ const DeletePatientModal = () => {
             className='Modal'
             ariaHideApp={false}
         >
-            <div className='modal-primary h-fit-content w-50 modal-container'>
-                delete patient
+            <div className='modal-primary h-fit-content w-40 modal-container'>
+                <div className='modal-header'>
+                    <span className='modal-title'>ATTENTION</span>
+                    <button className='close-button' onClick={() => closeDeletePatient()}>×</button>
+                </div>
+                <div className="modal-header-divider-2"></div>
+                <div className="modal-message">
+                    Are you sure you want to delete the selected patient (20240924418363)?.
+                </div>
+                <div className="d-flex gap-20px m-b-30 w-100 justify-content-center">
+                    <button type='submit' className='submit-button'>OK</button>
+                    <button type='submit' className='cancel-button'>CANCEL</button>
+                </div>
             </div>
 
         </ReactModal>
